b50da77d01254db2243ad723efb997ecefe9f6ea
Geoff Safcik <gsafcik@gmail.com>
2013-10-08 22:18:34 -0600

    Reworked the Observer based on excess code (duplication) of core Magento functionality. Removed that and sice using a hook, only included the important code to the module.
    Rework credit of this file goes to https://github.com/exobuzz.
    
    File Modified:
    
    - app/code/local/GSC/Stockavailability/Model/Observer.php

--- app/code/local/GSC/Stockavailability/Model/Observer.php
+++ app/code/local/GSC/Stockavailability/Model/Observer.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * @category    Graphic Sourcecode
- * @package     GSC_Stockavailability
- * @license     http://www.graphicsourcecode.com/gs-license.txt
- * @author      Geoff Safcik <info@graphicsourcecode.com>
- */
-
 class GSC_Stockavailability_Model_Observer
 {
     /**
@@ -17,43 +10,27 @@ class GSC_Stockavailability_Model_Observer
     public function refundOrderInventory($observer)
     {
         $creditmemo = $observer->getEvent()->getCreditmemo();
-        $items = array();
         foreach ($creditmemo->getAllItems() as $item) {
-            $return = false;
+            // check if item was to be returned to stock
+            $process = false;
             if ($item->hasBackToStock()) {
                 if ($item->getBackToStock() && $item->getQty()) {
-                    $return = true;
+                    $process = true;
                 }
             } elseif (Mage::helper('cataloginventory')->isAutoReturnEnabled()) {
-                $return = true;
+                $process = true;
             }
-            if ($return) {
-                if (isset($items[$item->getProductId()])) {
-                    $items[$item->getProductId()]['qty'] += $item->getQty();
-                } else {
-                    $items[$item->getProductId()] = array(
-                        'qty' => $item->getQty(),
-                        'item'=> null,
-                    );
+
+            if ($process == true) {
+                // update the product id - based on code from http://stackoverflow.com/questions/10453324/
+                $productId = $item->getProductId();
+                $product = Mage::getModel('catalog/product')->load($productId);
+
+                if (!$product->isConfigurable()) {
+                    $stockItem = $product->getStockItem();
+                    $stockItem->setIsInStock(1);
+                    $stockItem->save();
                 }
-            }
-        }
-        Mage::getSingleton('cataloginventory/stock')->revertProductsSale($items);
-
-        foreach ($creditmemo->getAllItems() as $item) {
-            $productId = $item->getProductId();
-            $product = Mage::getModel('catalog/product')->load($productId);
-
-            if(!$product->isConfigurable()){
-
-                $stockItem = $product->getStockItem();
-
-                $stockItem->setQty($item->getQty());
-                $stockItem->setIsInStock(1);
-                $stockItem->save();
-
-                //$product->setStockItem($stockItem);
-                //$product->save();
             }
         }
     }
